4afaa198bcbe8e483e42553b7ed103443eca59cb
Eric Nguyen <eric.nguyen@i-payout.com>
2022-07-29 15:38:58 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ID check thingy: ");
+        using (var reader = new StreamReader(@"C:\Users\eric.nguyen\Documents\test.csv"))
+        {
+            reader.ReadLine(); //ignore first line
+            List<string> listA = new List<string>();
+            //List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+                //var values = line.Split(';');
+
+                //listA.Add(values[0]);
+                //listB.Add(values[1]);
+            }
+        }
+    }
+}
