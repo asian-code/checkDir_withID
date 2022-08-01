c64b4584932c754f50c130980288c262370a1d74
Eric Nguyen <eric.nguyen@i-payout.com>
2022-08-01 17:02:20 -0400

    Finished app, everything works and updated

--- Program.cs
+++ Program.cs
@@ -3,20 +3,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Eric is the best IT guy! ");
         string[] targetDir = Directory.GetFiles(@"C:\Users\eric.nguyen\Documents");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(String.Format("[{0}] Files Found", targetDir.Length));
+        Console.ForegroundColor = ConsoleColor.White;
 
-        using (var reader = new StreamReader(@"C:\Users\eric.nguyen\Documents\test.csv"))
+        Console.WriteLine("Eric is the best IT guy!\n--- Starting scan ------------------------------");
+        using (var reader = new StreamReader(@"C:\Users\eric.nguyen\Documents\Programming\test.csv"))
         {
             bool seen = false;
             string dir = "";
+            int c=0;//# of IDs
             reader.ReadLine(); //ignore first line
 
             while (!reader.EndOfStream)// read ID file
             {
+                c++;
                 var id = reader.ReadLine();// stores ID
                 for (int i = 0; i < targetDir.Length; i++)
-                {//loop the folder
+                {//loop the files in folder
+                    seen = false;
                     using (StreamReader sr = new StreamReader(targetDir[i]))// check current file
                     {
                         string contents = sr.ReadToEnd();
@@ -30,22 +36,27 @@ class Program
                                 Console.ForegroundColor = ConsoleColor.Cyan;
                                 Console.WriteLine(" " + seen.ToString() + " - " + dir);
                                 Console.ForegroundColor = ConsoleColor.White;
+                                break;
                             }
-                            else
-                            {
-                                Console.Write(id);
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine(" " + seen.ToString());
-                                Console.ForegroundColor = ConsoleColor.White;
-                            }
-                            break;
+                   
+                            
                         }
                     }
                     
-                    seen = false;
+                    
                 }
+                if (!seen)
+                {
+                    Console.Write(id);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" " + seen.ToString());
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                
             }
-            
+            Console.WriteLine(String.Format("--- {0} IDs checked ------------------------------",c));
+
+
         }
     }
 }
